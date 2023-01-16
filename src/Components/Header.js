2d0e6d49478412dd765654986c37836ee24c3079
soubhagyasethy <ssethy017@gmail.com>
2023-01-16 23:59:24 +0530

    feat: added header

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return(
+        <header className="header">
+            <img src="/" alt="my profile"/>
+            <h1>Name</h1>
+            <h3>Designantion</h3>
+            <a href="/">website</a>
+            <div>Social media link</div>
+        </header>
+    )
+};
