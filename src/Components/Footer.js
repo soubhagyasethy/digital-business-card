e918ddf01c339d1c000ce0da520b9f4a102aed39
soubhagyasethy <ssethy017@gmail.com>
2023-01-17 22:42:40 +0530

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,13 @@
 import GitHub from "../Images/GitHub Icon.png"
 import Instagram from "../Images/Instagram Icon.png"
-import Linkedin from "../Images/linkedin.png"
+import Facebook from "../Images/Facebook Icon.png"
 import Twitter from "../Images/Twitter Icon.png"
 export default function Footer() {
     return(
         <nav className="footer">
             <li className="github"><a href="https://github.com/soubhagyasethy"><img src={GitHub} alt="social media links"/></a></li>
             <li className="instagram"><a href="https://www.instagram.com/soubhagya_._/"><img src={Instagram} alt="social media links"/></a></li>
-            <li className="linkedin"><a href="/"><img src={Linkedin} alt="social media links"/></a></li>
+            <li className="Facebook"><a href="/"><img src={Facebook} alt="social media links"/></a></li>
             <li className="twitter"><a href="https://twitter.com/soubhagyasethy3"><img src={Twitter} alt="social media links"/></a></li>
         </nav>
     )
