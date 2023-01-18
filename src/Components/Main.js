30493207691afef3625a4a769ffe1291c1bb8f09
soubhagyasethy <ssethy017@gmail.com>
2023-01-18 13:51:42 +0530

    style: updated main section

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -11,22 +11,22 @@ export default function Main() {
             </a>
 
             <div className="contact-button">
-            <div className="email">
+            <button className="email">
                 <img className="email-img" src={Email} alt="Email"/>
-                <a style={{textDecoration:"none", color:"black"}} href="/">Email</a>
-            </div>
+                <a style={{textDecoration:"none", color:"black"}} href="mailto:ssethy017@gmail.com">Email</a>
+            </button>
 
-            <div className="linkedin">
+            <button className="linkedin">
                <img className="linkedin-img" src={Linkedin} alt="Linkedin"/>
-               <a style={{textDecoration:"none", color:"white"}} href="/">Linkedin</a>
-            </div>   
+               <a style={{textDecoration:"none", color:"white"}} href="https://www.linkedin.com/in/soubhagya-sethy-3b5404182/">Linkedin</a>
+            </button>   
             </div>
             
 
             <h2 className="about">About</h2>
-            <p className="paragraph">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className="paragraph">I am a frontend developer with a particular interest in making user friendly responsive web apps. I try to keep up with new technologies and best practices, and am always looking for new things to learn.</p>
             <h2 className="intrests">Intrests</h2>
-            <p className="paragraph">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p className="paragraph">Movies. Comicbook. Pop culture ninja. Food Expert. Internet fanatic. Travel geek. Coffee fanatic.</p>
         </main>
     )
 };
